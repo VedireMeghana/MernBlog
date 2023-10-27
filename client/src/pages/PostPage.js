9a822f1fce3efe3cba34131f2dc768395065335a
VedireMeghana <113010068+VedireMeghana@users.noreply.github.com>
2023-10-27 21:19:07 +0530

    Update PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -15,6 +15,28 @@ export default function PostPage() {
       });
     });
   }, []);
+    const handleDeletePost = () => {
+    // Make an API request to delete the post on the server
+    fetch(`http://localhost:5000/post/${id}`, {
+      method: "DELETE",
+      credentials:'include'
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Post deleted successfully, navigate to the IndexPage
+          // history.push("/index");
+           navigate('/index')
+         
+        } else {
+          // Handle error here, e.g., show an alert
+          alert("Failed to delete the post.");
+        }
+      })
+      .catch((error) => {
+        // Handle any network or other errors
+        console.error("Error deleting post:", error);
+      });
+  };
   if (postInfo === null) return "";
   return (
     <div className="post-page">
@@ -40,6 +62,14 @@ export default function PostPage() {
             </svg>
             Edit this post
           </Link>
+          <br></br>
+          <br></br>
+          <button onClick={handleDeletePost} className="delete-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+          </svg>
+            Delete this post
+        </button>
         </div>
       )}
 
